20b5de398a3caf6ee9d81f9b651214e59889e293
Victor Moreira <victorfranklyn@poli.ufrj.br>
2020-02-25 18:45:45 -0300

    Valor acrescentado, data e valor no lugar certo

--- im_teste.py
+++ im_teste.py
@@ -18,11 +18,11 @@ POSICAO_DA_NUMERACAO_2 = (530,302)
 MENSAGEM_DO_PREMIO = "Um grande prêmio"
 POSICAO_DO_PREMIO = (550,18)
 
-MENSAGEM_DA_DATA = "04/04/2020"
-POSICAO_DA_DATA = (1080,360)
+MENSAGEM_DA_DATA = "04/04/20"
+POSICAO_DA_DATA = (800,300)
 
-MENSAGEM_DO_VALOR = "R$1.400,00"
-POSICAO_DO_VALOR = ( 1000,POSICAO_DA_DATA[1] )
+MENSAGEM_DO_VALOR = "R$2,00"
+POSICAO_DO_VALOR = ( 990,POSICAO_DA_DATA[1] )
 
 class texto:
 	def __init__(self, mensagem, posicao):
@@ -43,6 +43,8 @@ image = Image.open(NOME_DA_IMAGEM_ORIGINAL)
 
 premio = texto(MENSAGEM_DO_PREMIO, POSICAO_DO_PREMIO)
 data = texto( MENSAGEM_DA_DATA, POSICAO_DA_DATA)
+valor = texto( MENSAGEM_DO_VALOR, POSICAO_DO_VALOR)
+
 
 for i in range(1, NUMERO_DE_COPIAS + 1):
 	copy = image.copy()
@@ -59,7 +61,7 @@ for i in range(1, NUMERO_DE_COPIAS + 1):
 	draw.text(*numeracao_2.get_parametros_do_draw() )
 	draw.text(*premio.get_parametros_do_draw() )
 	draw.text(*data.get_parametros_do_draw() )
-	
+	draw.text(*valor.get_parametros_do_draw() )
 
 	#copy.show()
 	filename = NOME_DAS_IMAGENS_DE_SAIDA + str(numeracao) + "." + FORMATO;
