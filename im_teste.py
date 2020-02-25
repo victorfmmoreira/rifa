7c9590a34dabbe43bb9a484172716436aab99cfc
Victor Moreira <victorfranklyn@poli.ufrj.br>
2020-02-25 17:25:40 -0300

    Classes usadas para mudar colocar as numerações

--- im_teste.py
+++ im_teste.py
@@ -2,6 +2,12 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+ARQUIVO_DA_FONTE = "Best in class.otf"
+TAMANHO_DA_FONTE = 24
+COR_DA_FONTE = "red"
+FORMATO = "png"
+NUMERO_DE_DIGITOS = "3"
+
 NUMERO_DE_COPIAS = 1
 NOME_DA_IMAGEM_ORIGINAL = "rifa.png"
 NOME_DAS_IMAGENS_DE_SAIDA = "rifa"
@@ -18,10 +24,6 @@ POSICAO_DA_DATA = (1080,360)
 MENSAGEM_DO_VALOR = "R$1.400,00"
 POSICAO_DO_VALOR = ( 1000,POSICAO_DA_DATA[1] )
 
-ARQUIVO_DA_FONTE = "Best in class.otf"
-TAMANHO_DA_FONTE = 24
-COR_DA_FONTE = "red"
-
 class texto:
 	def __init__(self, mensagem):
 		self.mensagem = mensagem
@@ -31,8 +33,11 @@ class texto:
 	tamanho = TAMANHO_DA_FONTE
 	cor_da_fonte = COR_DA_FONTE
 
-	def get_parametros_do_draw(): 
-		return [self.posicao, self.mensagem, self.cor_da_fonte,  self.arquivo_da_fonte, ] 
+	def get_font(self):
+		return ImageFont.truetype(self.arquivo_da_fonte, self.tamanho)
+
+	def get_parametros_do_draw(self): 
+		return [self.posicao, self.mensagem, self.cor_da_fonte, self.get_font() ] 
 
 image = Image.open(NOME_DA_IMAGEM_ORIGINAL)
 
@@ -40,14 +45,20 @@ for i in range(1, NUMERO_DE_COPIAS + 1):
 	copy = image.copy()
 
 	draw = ImageDraw.Draw(copy)
-	font = ImageFont.truetype(ARQUIVO_DA_FONTE, TAMANHO_DA_FONTE)
-	numeracao = "%.3i" % i
+	numeracao = ("%." + NUMERO_DE_DIGITOS + "i") % i
 	text = str(numeracao)
-	draw.text(POSICAO_DA_NUMERACAO_1, text, COR_DA_FONTE, font=font)
-	draw.text(POSICAO_DA_NUMERACAO_2, text, COR_DA_FONTE, font=font)
+
+	numeracao_1 = texto(text)
+	numeracao_1.posicao = POSICAO_DA_NUMERACAO_1
+
+	numeracao_2 = texto(text)
+	numeracao_2.posicao = POSICAO_DA_NUMERACAO_2
+
+	draw.text(*numeracao_1.get_parametros_do_draw() )
+	draw.text(*numeracao_2.get_parametros_do_draw() )
 
 	#copy.show()
-	filename = NOME_DAS_IMAGENS_DE_SAIDA + str(numeracao) + ".png";
-	copy.save(filename, "png")
+	filename = NOME_DAS_IMAGENS_DE_SAIDA + str(numeracao) + "." + FORMATO;
+	copy.save(filename, FORMATO)
 	copy.close()
 
